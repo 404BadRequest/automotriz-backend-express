0ca98e9a9373787050497bc031e2ef893e16ce22
Julio Soto <ju.soto.sanchez@gmail.com>
2021-02-16 00:46:09 -0300

    Rutas

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/welcome", (req, res) => {
     res.send("Welcome to world");
 });
 
-app.listen(3000, () => {
-    console.log("Listen on port 3000");
+app.get("/test", (req, res) => {
+    res.send("<h1>Test<h1>");
+});
+
+app.listen(5000, () => {
+    console.log("Listen on port 5000");
 });
